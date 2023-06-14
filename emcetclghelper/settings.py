43c0400b51b730f863b2dc5d34e27749579e8260
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2023-06-14 11:10:13 +0530

    Update settings.py

--- emcetclghelper/settings.py
+++ emcetclghelper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gsczna$!kgtfe$)jttl68%_)qoohm%1ia8*fo3$_4amwfxx3o%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'emcetclghelper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -120,3 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='media/'
+
+import os 
+
+# At the end of file. add these lines
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
